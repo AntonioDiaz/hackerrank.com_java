4f01b7998b7599004019ea49b52461864511f8c2
AntonioDiaz <antonio.diaz.arroyo@gmail.com>
2016-06-12 18:26:47 +0200

    gemStones and game of thrones practices.

---
+++ src/easy/GameOfThrones01.java
@@ -0,0 +1,40 @@
+package easy;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Scanner;
+
+public class GameOfThrones01 {
+
+	
+	public static void main(String[] args) throws IOException {
+		InputStream input = GameOfThrones01.class.getClassLoader().getResourceAsStream("config.properties");
+		Properties properties = new Properties();
+		properties.load(input);
+		File file = new File(properties.getProperty("input.path") + "game_of_trones_01.txt");
+		Scanner in = new Scanner(file);
+		// Scanner in = new Scanner(System.in);
+		String str = in.next();
+		byte[] bytes = str.getBytes();
+		Map<Byte, Integer> map = new HashMap<Byte, Integer>();
+		for (byte b : bytes) {
+			int repetitions = 1;
+			if (map.containsKey(b)) {
+				repetitions = map.get(b) + 1;
+			}
+			map.put(b, repetitions);
+		}
+		int odd = 0;
+		for (Byte b : map.keySet()) {
+			if (map.get(b)%2!=0) {
+				odd ++;
+			}
+		}
+		System.out.println(odd<=1?"YES":"NO");
+		in.close();
+	}
+}
