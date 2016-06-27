27f71cb515e45bfdde7b00584590533d74a3b47a
AntonioDiaz <antonio.diaz.arroyo@gmail.com>
2016-06-27 20:24:14 +0200

    remove warnings

--- src/security/easy/KeywordTranspositionCipher.java
+++ src/security/easy/KeywordTranspositionCipher.java
@@ -5,9 +5,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Properties;
 import java.util.Scanner;
 
