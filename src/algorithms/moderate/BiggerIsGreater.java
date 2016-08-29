04364cb3d3719114ba32b163a2344a24537e2b6a
AntonioDiaz <antonio.diaz.arroyo@gmail.com>
2016-08-29 20:19:25 +0200

    bigger is greatter!

---
+++ src/algorithms/moderate/BiggerIsGreater.java
@@ -0,0 +1,69 @@
+package algorithms.moderate;
+
+import java.io.File;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.Scanner;
+
+public class BiggerIsGreater {
+
+	public static void main(String[] args) throws Exception{
+		InputStream input = BiggerIsGreater.class.getClassLoader().getResourceAsStream("config.properties");
+		Properties properties = new Properties();
+		properties.load(input);
+		File file = new File(properties.getProperty("input.path") + "bigger_is_greater.txt");
+		Scanner in = new Scanner(file);
+		//Scanner in = new Scanner(System.in);
+		int testCasesCount = in.nextInt();
+		for (int i = 0; i < testCasesCount; i++) {
+			String word = in.next();
+			byte[] bytes = word.getBytes();
+			if (!BiggerIsGreater.nextPermutation(bytes)) {
+				System.out.println("no answer");
+			} else {
+				System.out.println(new String(bytes));
+			}
+		}
+		in.close();
+
+	}
+	
+	
+	private static boolean nextPermutation(byte[] array) {
+	    // Find longest non-increasing suffix
+	    int i = array.length - 1;
+	    while (i > 0 && array[i - 1] >= array[i])
+	        i--;
+	    // Now i is the head index of the suffix
+	    
+	    // Are we at the last permutation already?
+	    if (i <= 0)
+	        return false;
+	    
+	    // Let array[i - 1] be the pivot
+	    // Find rightmost element that exceeds the pivot
+	    int j = array.length - 1;
+	    while (array[j] <= array[i - 1])
+	        j--;
+	    // Now the value array[j] will become the new pivot
+	    // Assertion: j >= i
+	    
+	    // Swap the pivot with j
+	    byte temp = array[i - 1];
+	    array[i - 1] = array[j];
+	    array[j] = temp;
+	    
+	    // Reverse the suffix
+	    j = array.length - 1;
+	    while (i < j) {
+	        temp = array[i];
+	        array[i] = array[j];
+	        array[j] = temp;
+	        i++;
+	        j--;
+	    }
+	    
+	    // Successfully computed the next permutation
+	    return true;
+	}
+}
