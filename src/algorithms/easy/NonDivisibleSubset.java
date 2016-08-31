a46ffa1385a609809083c54bedb17aad73099abb
AntonioDiaz <antonio.diaz.arroyo@gmail.com>
2016-08-31 18:21:50 +0200

    non divisible subset

--- src/algorithms/easy/NonDivisibleSubset.java
+++ src/algorithms/easy/NonDivisibleSubset.java
@@ -2,10 +2,10 @@ package algorithms.easy;
 
 import java.io.File;
 import java.io.InputStream;
-import java.util.HashSet;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 import java.util.Scanner;
-import java.util.Set;
 
 public class NonDivisibleSubset {
 
@@ -15,23 +15,33 @@ public class NonDivisibleSubset {
 		properties.load(input);
 		File file = new File(properties.getProperty("input.path") + "non_divisible_subset.txt");
 		Scanner in = new Scanner(file);
-		//Scanner in = new Scanner(System.in);
+		// Scanner in = new Scanner(System.in);
 		int n = in.nextInt();
 		int k = in.nextInt();
-		int[] values = new int[n];
-		Set<Integer> subSet = new HashSet<Integer>();
+		Map<Integer, Integer> remaindersCount = new HashMap<Integer, Integer>();
+		for (int i = 0; i < k; i++) {
+			remaindersCount.put(i, 0);
+		}
 		for (int i = 0; i < n; i++) {
-			values[i] = in.nextInt();
+			int remainder = in.nextInt() % k;
+			remaindersCount.put(remainder, remaindersCount.get(remainder) + 1);
 		}
-		for (int i = 0; i < values.length; i++) {
-			for (int j = i+1; j < values.length; j++) {
-				if ((values[i] + values[j])%k!=0) {
-					subSet.add(values[i]);
-					subSet.add(values[j]);
+		// System.out.println(remaindersCount);
+		int maxSubSetCount = 0;
+		for (int i = 0; i <= (k / 2); i++) {
+			if (i==0 || i==(k-i)) {
+				if (remaindersCount.get(i)>0) {
+					maxSubSetCount += 1;
 				}
-			}			
+			} else {
+				if (remaindersCount.get(i) > remaindersCount.get(k - i)) {
+					maxSubSetCount += remaindersCount.get(i);
+				} else {
+					maxSubSetCount += remaindersCount.get(k - i);
+				}
+			}
 		}
-		System.out.println(subSet.size());
+		System.out.println(maxSubSetCount);
 		in.close();
 	}
 }
