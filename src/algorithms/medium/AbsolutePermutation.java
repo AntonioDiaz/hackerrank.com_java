e12661e3be6a7166149b8cecf43c5ab583085a4e
AntonioDiaz <antonio.diaz.arroyo@gmail.com>
2016-09-11 12:11:46 +0200

    Absolute permutation

--- src/algorithms/medium/AbsolutePermutation.java
+++ src/algorithms/medium/AbsolutePermutation.java
@@ -1,7 +1,10 @@
 package algorithms.medium;
 
+import java.io.File;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 import java.util.Scanner;
 
 public class AbsolutePermutation {
@@ -13,53 +16,67 @@ public class AbsolutePermutation {
 		this.n = n;
 		this.k = k;
 	}
-
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
+	public static void main (String[] args) throws Exception{
+		InputStream input = AbsolutePermutation.class.getClassLoader().getResourceAsStream("config.properties");
+		Properties properties = new Properties();
+		properties.load(input);
+		File file = new File(properties.getProperty("input.path") + "absolute_position_09.txt");
+		File fileOutput = new File(properties.getProperty("input.path") + "absolute_position_09_output.txt");
+		Scanner in = new Scanner(file);
+		Scanner out = new Scanner(fileOutput);
+		//Scanner in = new Scanner(System.in);
 		int testCases = in.nextInt();
 		for (int i = 0; i < testCases; i++) {
+			String permutationsExpected = out.nextLine();			
+			System.out.println(permutationsExpected);
 			int n = in.nextInt();
 			int k = in.nextInt();
 			AbsolutePermutation absolutePermutation = new AbsolutePermutation(n, k);
-			System.out.println(listToString(absolutePermutation.calculatePermutation()));
+			List<Integer> permutation = absolutePermutation.calculatePermutation();
+			String strPermutation = listToString(permutation);
+			System.out.println(permutationsExpected.equals(strPermutation));
 		}
 		in.close();
+		out.close();
 	}
 
-	/**
-	 * -If n is an even number (n%k == 0) lets you know if there is a valid permutation or you should just print -1
-	 * -If k=0 just print 1... n
-	 * -If n is an odd number and it also is greater than 0, print -1
-	 * @return
-	 */	
-	
+	/** -If n is an even number (n%k == 0) lets you know if there is a valid permutation or you should just print -1 -If k=0 just print 1...
+	 * n -If n is an odd number and it also is greater than 0, print -1
+	 * 
+	 * @return */
+
 	public List<Integer> calculatePermutation() {
 		List<Integer> permutation = new ArrayList<Integer>();
-		if (this.k == 0) {
-			for (int i = 0; i < n; i++) {
-				permutation.add(i + 1);
+		if (k==0) {
+			for (int i = 1; i <= this.n; i++) {
+				permutation.add(i);
 			}
-		} else if (this.k == n - 1 && k==1 ) {
-			for (int i = n - 1; i >= 0; i--) {
-				permutation.add(i + 1);
-			}
+		} else if (n%k==0) {
+			for (int i = 1; i <= this.n; i++) {
+				int pos = i - k;
+				int iTaken = pos - k;
+				if (pos <= 0 || (iTaken > 0 && permutation.get(iTaken-1) == pos)) {
+					pos = i + k;
+				}
+				permutation.add(pos);
+			}			
 		} else {
 			permutation.add(-1);
 		}
 		return permutation;
 	}
 
-	private static String listToString(List<Integer> list) {
-		String strConverted = "";
+	public static String listToString(List<Integer> list) {
+		StringBuilder strConverted = new StringBuilder("");
 		for (int i = 0; i < list.size(); i++) {
-			strConverted += list.get(i);
-			if (i<list.size()-1) {
-				strConverted += " ";
+			strConverted.append(list.get(i));
+			if (i < list.size() - 1) {
+				strConverted.append(" ");
 			}
 		}
-		return strConverted;
+		return strConverted.toString();
 	}
-	
+
 	public Integer getN() {
 		return n;
 	}
--- test/algorithms/medium/AbsolutePermutationTest.java
+++ test/algorithms/medium/AbsolutePermutationTest.java
@@ -1,10 +1,8 @@
 package algorithms.medium;
 
-import java.util.List;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
 
 public class AbsolutePermutationTest {
 
@@ -12,36 +10,25 @@ public class AbsolutePermutationTest {
 	@Test
 	public void calculatePermutation01(){
 		AbsolutePermutation absolutePermutation = new AbsolutePermutation(2, 1);
-		assertEquals("2 1", this.listToString(absolutePermutation.calculatePermutation()));
+		assertEquals("2 1", AbsolutePermutation.listToString(absolutePermutation.calculatePermutation()));
 	}
 	
 	@Test
 	public void calculatePermutation02(){
 		AbsolutePermutation absolutePermutation = new AbsolutePermutation(3, 0);
-		assertEquals("1 2 3", this.listToString(absolutePermutation.calculatePermutation()));
+		assertEquals("1 2 3", AbsolutePermutation.listToString(absolutePermutation.calculatePermutation()));
 	}
 	
 	@Test
 	public void calculatePermutation03(){
 		AbsolutePermutation absolutePermutation = new AbsolutePermutation(3, 2);
-		assertEquals("-1", this.listToString(absolutePermutation.calculatePermutation()));
+		assertEquals("-1", AbsolutePermutation.listToString(absolutePermutation.calculatePermutation()));
 	}
 	
-	private static final String OUTPUT_04 = "3 4 1 2 7 8 5 6 11 12 9 10 15 16 13 14 19 20 17 18 23 24 21 22 27 28 25 26 31 32 29 30 35 36 33 34 39 40 37 38 43 44 41 42 47 48 45 46 51 52 49 50 55 56 53 54 59 60 57 58 63 64 61 62 67 68 65 66 71 72 69 70 75 76 73 74 79 80 77 78 83 84 81 82 87 88 85 86 91 92 89 90 95 96 93 94 99 100 97 98 ";
+	private static final String OUTPUT_04 = "3 4 1 2 7 8 5 6 11 12 9 10 15 16 13 14 19 20 17 18 23 24 21 22 27 28 25 26 31 32 29 30 35 36 33 34 39 40 37 38 43 44 41 42 47 48 45 46 51 52 49 50 55 56 53 54 59 60 57 58 63 64 61 62 67 68 65 66 71 72 69 70 75 76 73 74 79 80 77 78 83 84 81 82 87 88 85 86 91 92 89 90 95 96 93 94 99 100 97 98";
 	@Test
 	public void calculatePermutation04(){
 		AbsolutePermutation absolutePermutation = new AbsolutePermutation(100, 2);
-		assertEquals(OUTPUT_04, this.listToString(absolutePermutation.calculatePermutation()));
-	}
-	
-	private String listToString(List<Integer> list) {
-		String strConverted = "";
-		for (int i = 0; i < list.size(); i++) {
-			strConverted += list.get(i);
-			if (i<list.size()-1) {
-				strConverted += " ";
-			}
-		}
-		return strConverted;
-	}
+		assertEquals(OUTPUT_04, AbsolutePermutation.listToString(absolutePermutation.calculatePermutation()));
+	}	
 }
