3994d3e46a7ce412beb9d1b6f7405751b79a8d56
AntonioDiaz <antonio.diaz.arroyo@gmail.com>
2016-12-19 19:04:26 +0100

    Challenge: super reduced string.

---
+++ src/algorithmos/strings/easy/SuperReducedString.java
@@ -0,0 +1,49 @@
+package algorithmos.strings.easy;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.Scanner;
+
+public class SuperReducedString {
+
+	public static void main(String[] args) throws IOException {
+		InputStream inputStream = SuperReducedString.class.getClassLoader().getResourceAsStream("config.properties");
+		Properties properties = new Properties();
+		properties.load(inputStream);
+		File file = new File(properties.getProperty("input.path") + "super_reduced_string.txt");
+		Scanner in = new Scanner(file);
+		// Scanner in = new Scanner(System.in);
+		String input = in.nextLine();
+		String reducedString = SuperReducedString.calculateReducedString(input);
+		System.out.println(reducedString);
+		in.close();
+	}
+
+	public static String calculateReducedString(String input) {
+		boolean pairFound = true;
+		String inputAux = "";
+		while (pairFound) {
+			inputAux = "";
+			pairFound = false;
+			int i = 0;
+			while (i < input.length()) {
+				if (i + 1 < input.length()) {
+					if (input.charAt(i) == input.charAt(i + 1)) {
+						i += 2;
+						pairFound = true;
+					} else {
+						inputAux += input.charAt(i);
+						i++;
+					}
+				} else {
+					inputAux += input.charAt(i);
+					i++;
+				}
+			}
+			input = inputAux;
+		}
+		return inputAux == "" ? "Empty String" : inputAux;
+	}
+}
---
+++ test/algorithmos/strings/easy/SuperReducedStringTest.java
@@ -0,0 +1,33 @@
+package algorithmos.strings.easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SuperReducedStringTest {
+
+	@Test
+	public void test01(){
+		final String input = "aaabccddd";
+		final String expected = "abd";
+		String obtained = SuperReducedString.calculateReducedString(input);
+		assertEquals("TestCase 01 failed", expected, obtained);
+	}
+	
+	@Test
+	public void test02(){
+		final String input = "baab";
+		final String expected = "Empty String";
+		String obtained = SuperReducedString.calculateReducedString(input);
+		assertEquals("TestCase 02 failed", expected, obtained);
+	}
+	
+	@Test
+	public void test03(){
+		final String expected = "aa";
+		final String input = "Empty String";
+		String obtained = SuperReducedString.calculateReducedString(input);
+		assertEquals("TestCase 03 failed", expected, obtained);
+	}
+	
+}
