b3c35a29375a05c8ec2ff4cf032262c2ab979540
AntonioDiaz <antonio.diaz.arroyo@gmail.com>
2016-12-21 21:20:58 +0100

    Challenge 03: Code Week 27

--- src/contest/weekcode27/HackonacciMatrixRotations.java
+++ src/contest/weekcode27/HackonacciMatrixRotations.java
@@ -5,6 +5,7 @@ package contest.weekcode27;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.math.BigInteger;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
@@ -20,42 +21,42 @@ public class HackonacciMatrixRotations {
 		// Scanner in = new Scanner(System.in);
 		int n = in.nextInt();
 		int q = in.nextInt();
-		boolean previous[] = new boolean[3];
-		previous[0] = true; // 1
-		previous[1] = false; // 2
-		previous[2] = true; // 3
 		Map<Integer, Boolean> map = new HashMap<Integer, Boolean>();
+		/* found that this series is repeated, 
+		 * example parity (hackonacci(4)) == parity (hackonacci(11))   */		
+		map.put(1, true); 
+		map.put(2, false);
+		map.put(3, true);
+		map.put(4, false);
+		map.put(5, false);
+		map.put(6, true);
 		map.put(0, true);
-		map.put(1, false);
-		/** each hackonacci element depends of the 3 previous it is necessary keep trak of the last 3 */
-		int maxPosible = (new Double(Math.pow(n * n, 2))).intValue();
-		for (int i = 3; i <= maxPosible; i++) {
-			previous[i % 3] = previous[(i - 1) % 3] ^ previous[(i - 3) % 3];
-			if (Math.sqrt(i) % 1 == 0) {
-				map.put(i, previous[i % 3]);
-			}
-		}
-		// System.out.println(Arrays.asList(queries));
 		int[] possibleResults = new int[4];
 		possibleResults[0] = 0;
 		for (int i = 1; i <= n; i++) {
 			for (int j = 1; j <= n; j++) {
-				Integer valueOriginal = new Double(Math.pow(i * j, 2)).intValue();
-				for (int k = 1; k <= 3; k++) {					
+				BigInteger bigInteger = BigInteger.valueOf(i).multiply(BigInteger.valueOf(j));
+				bigInteger = bigInteger.pow(2);
+				bigInteger = bigInteger.mod(BigInteger.valueOf(7));
+				int valueMod7 = bigInteger.intValue();
+				for (int k = 1; k <= 3; k++) {
 					int iNew = 0;
 					int jNew = 0;
-					if (k == 1) {
+					if (k == 3) {
 						iNew = n - i + 1;
 						jNew = j;
 					} else if (k == 2) {
 						iNew = n - i + 1;
 						jNew = n - j + 1;
-					} else if (k == 3) {
+					} else if (k == 1) {
 						iNew = j;
 						jNew = n - i + 1;
 					}
-					Integer valueRotation = new Double(Math.pow(iNew * jNew, 2)).intValue();
-					boolean areDistinct = !map.get(valueOriginal) == map.get(valueRotation);
+					BigInteger bigIntegerRotation = BigInteger.valueOf(iNew).multiply(BigInteger.valueOf(jNew));
+					bigIntegerRotation = bigIntegerRotation.pow(2);
+					bigIntegerRotation = bigIntegerRotation.mod(BigInteger.valueOf(7));
+					int valueMod7Rotation = bigIntegerRotation.intValue();
+					boolean areDistinct = !map.get(valueMod7) == map.get(valueMod7Rotation);
 					if (areDistinct) {
 						possibleResults[k]++;
 					}
@@ -63,7 +64,7 @@ public class HackonacciMatrixRotations {
 			}
 		}
 		for (int i = 0; i < q; i++) {
-			int rotationsSimplified = (in.nextInt() / 90)%4;
+			int rotationsSimplified = (in.nextInt() / 90) % 4;
 			System.out.println(possibleResults[rotationsSimplified]);
 		}
 		in.close();
