95b43c96dfa5ccb9ee746456710575fb39a7dab5
Antonio Diaz <antonio.diaz.arroyo@gmail.com>
2016-12-23 12:12:07 +0100

    Challenge: insertion sort part 02 (improvement).

--- src/algorithms/sorting/easy/InsertionSort02Part.java
+++ src/algorithms/sorting/easy/InsertionSort02Part.java
@@ -1,4 +1,5 @@
 package algorithms.sorting.easy;
+
 import java.io.File;
 import java.io.InputStream;
 import java.util.ArrayList;
@@ -7,25 +8,25 @@ import java.util.Properties;
 import java.util.Scanner;
 
 public class InsertionSort02Part {
-    
-    
+
 	public static void main(String[] args) throws Exception {
 		InputStream input = InsertionSort02Part.class.getClassLoader().getResourceAsStream("config.properties");
 		Properties properties = new Properties();
 		properties.load(input);
 		File file = new File(properties.getProperty("input.path") + "insertion_sort_02_part.txt");
 		Scanner scanner = new Scanner(file);
-		//Scanner scanner = new Scanner(System.in);
+		// Scanner scanner = new Scanner(System.in);
 		List<Integer> list = new ArrayList<Integer>();
 		int s = scanner.nextInt();
 		for (int i = 0; i < s; i++) {
 			int n = scanner.nextInt();
 			list.add(n);
 		}
-		for (int i=2; i<list.size() + 1; i++) {
-			list = sortElement(list, i);
-			InsertionSort02Part.printList(list);
-		}
+		InsertionSort02Part.sortList(list);
+		// for (int i=2; i<list.size() + 1; i++) {
+		// list = sortElement(list, i);
+		// InsertionSort02Part.printList(list);
+		// }
 		scanner.close();
 	}
 
@@ -33,7 +34,25 @@ public class InsertionSort02Part {
 		for (Integer integer : list) {
 			System.out.print(integer + " ");
 		}
-		System.out.println("");		
+		System.out.println("");
+	}
+
+	private static int sortList(List<Integer> list) {
+		int count = 0;
+		for (int i = 1; i < list.size(); i++) {
+			int newIndex = i - 1;
+			int oldIndex = i;
+			while (newIndex >= 0 && list.get(oldIndex) < list.get(newIndex)) {
+				int aux = list.get(newIndex);
+				list.set(newIndex, list.get(oldIndex));
+				list.set(oldIndex, aux);
+				oldIndex = newIndex;
+				newIndex--;
+				count++;
+			}
+			printList(list);
+		}
+		return count;
 	}
 
 	private static List<Integer> sortElement(List<Integer> list, int mySize) {
@@ -41,7 +60,7 @@ public class InsertionSort02Part {
 		int element = list.get(mySize - 1);
 		int index = mySize - 2;
 		while (!sorted) {
-			if (index>=0 && list.get(index) > element) {
+			if (index >= 0 && list.get(index) > element) {
 				list.set(index + 1, list.get(index));
 				index--;
 			} else {
